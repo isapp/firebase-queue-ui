674db7bfc7c6f36dff25de2f59db9d8f945be7ce
Pedro Felipe <pedro@pedrofelipe.com.br>
2016-10-14 14:50:04 -0300

    add togglemodifier lib

--- app/scripts/card.js
+++ app/scripts/card.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import $ from 'jquery';
+import 'jQuery.toggleModifier'
 
 $('.js-card-queue-toggle').click(function (e) {
 
   e.preventDefault();
 
-  $(this).closest('.js-card-queue').toggleClass('mdl-card--expanded');
+  $(this).closest('.js-card-queue').toggleModifier('expanded');
 });
